38269c68bca399f44d776a9dbaf3d0f42201e5d9
Xyberista <50119873+Xyberista@users.noreply.github.com>
2022-04-30 08:21:52 -0500

    more term functionality

--- src/term.rs
+++ src/term.rs
@@ -4,6 +4,8 @@ pub struct Term {
     pub term: String,
     pub book_definition: String,
     pub user_definition: String,
+    pub example: String,
+    pub picture_link: String,
 }
 
 impl Term {
@@ -12,6 +14,28 @@ impl Term {
     }
 
     pub fn filename(&self) -> String {
-        self.term.replace(' ', "_") + ".md"
+        format!("{}_{}{}", self.id, self.term.replace(' ', "_"), ".md")
+    }
+
+    pub fn picture_link(&self) -> String {
+        self.picture_link.trim().to_string()
+    }
+
+    pub fn picture_name(&self, extension: &str) -> String {
+        format!("{}_{}.{}", self.id, self.term.replace(' ', "_"), extension)
+    }
+
+    pub fn picture_extension(&self) -> String {
+        self.picture_link.rsplit('.').next().unwrap().to_string()
+    }
+}
+
+impl std::fmt::Display for Term {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Term:\n{}", self.term)?;
+        writeln!(f, "Book Definition:\n{:?}", self.book_definition)?;
+        writeln!(f, "User Definition:\n{}", self.user_definition)?;
+        writeln!(f, "Example:\n{}", self.example)?;
+        Ok(())
     }
 }
